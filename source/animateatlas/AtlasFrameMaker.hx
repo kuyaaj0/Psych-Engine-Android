ae57881b6079ac476ea23167ed5b80e74002b4a9
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2023-08-01 05:41:13 +0800

    Update AtlasFrameMaker.hx

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -51,9 +51,9 @@ class AtlasFrameMaker extends FlxFramesCollection
 		var graphic:FlxGraphic = Paths.image('$key/spritemap');
 		var ss:SpriteAnimationLibrary = new SpriteAnimationLibrary(animationData, atlasData, graphic.bitmap);
 		var t:SpriteMovieClip = ss.createAnimation(noAntialiasing);
-		if(_excludeArray == 'null<string>');
+		if(_excludeArray == null)<string>
 		{
-			_excludeArray = t.getFrameLabels();
+			_excludeArray = 't.getFrameLabels'();
 			//trace('creating all anims');
 		}
 		trace('Creating: ' + _excludeArray);
