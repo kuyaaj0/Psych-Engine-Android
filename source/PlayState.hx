13b733c9dfd882c8a198abf6aa62cb8f15f506e1
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2023-08-22 16:00:40 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3152,8 +3152,10 @@ class PlayState extends MusicBeatState
 
 		/*if (controls.PAUSE #if android || FlxG.android.justReleased.BACK #end && startedCountdown && canPause)
 		*/
-		{
+
 			var ret:Dynamic = callOnLuas('onPause', []);
+			{
+
 			if(ret != FunkinLua.Function_Stop) {
 				persistentUpdate = false;
 				persistentDraw = true;
@@ -3180,6 +3182,7 @@ class PlayState extends MusicBeatState
 				#end
 			}
 		}
+			}
 
 		if (FlxG.keys.anyJustPressed(debugKeysChart) && !endingSong && !inCutscene && SONG.song.toLowerCase() != 'vexation-hell')
 		{
